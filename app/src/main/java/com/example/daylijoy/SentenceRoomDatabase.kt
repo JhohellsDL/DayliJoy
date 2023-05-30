0923de8ab4de9fa872ee6226e6c1023f8e8f1a45
Jhohells <jhohellserick@gmail.com>
2023-05-30 17:31:17 -0500

    room database created

---
+++ app/src/main/java/com/example/daylijoy/SentenceRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.daylijoy
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.daylijoy.data.daos.SentenceDao
+import com.example.daylijoy.data.entities.SentenceEntity
+
+@Database(entities = [SentenceEntity::class], version = 1, exportSchema = false)
+abstract class SentenceRoomDatabase: RoomDatabase() {
+
+    abstract fun sentenceDao(): SentenceDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SentenceRoomDatabase? = null
+
+        fun getDatabase(context: Context): SentenceRoomDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SentenceRoomDatabase::class.java,
+                    "sentence_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
