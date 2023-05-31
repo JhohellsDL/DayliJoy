9582c92fe7e9f50c19f3902f5867849a0769324a
Jhohells <jhohellserick@gmail.com>
2023-05-30 22:43:40 -0500

    data base callback add, and create and add sentences manual

--- app/src/main/java/com/example/daylijoy/DailyJoyApplication.kt
+++ app/src/main/java/com/example/daylijoy/DailyJoyApplication.kt
@@ -3,11 +3,15 @@ package com.example.daylijoy
 import android.app.Application
 import com.example.daylijoy.data.SentenceRoomDatabase
 import com.example.daylijoy.data.respositories.SentenceRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class DailyJoyApplication : Application() {
 
+    val applicationScope = CoroutineScope(SupervisorJob())
+
     val database by lazy {
-        SentenceRoomDatabase.getDatabase(this)
+        SentenceRoomDatabase.getDatabase(this, applicationScope)
     }
 
     val repository by lazy {
--- app/src/main/java/com/example/daylijoy/data/SentenceRoomDatabase.kt
+++ app/src/main/java/com/example/daylijoy/data/SentenceRoomDatabase.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.daylijoy.data.daos.SentenceDao
 import com.example.daylijoy.data.entities.SentenceEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [SentenceEntity::class], version = 1, exportSchema = false)
-abstract class SentenceRoomDatabase: RoomDatabase() {
+abstract class SentenceRoomDatabase : RoomDatabase() {
 
     abstract fun sentenceDao(): SentenceDao
 
@@ -17,16 +20,51 @@ abstract class SentenceRoomDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: SentenceRoomDatabase? = null
 
-        fun getDatabase(context: Context): SentenceRoomDatabase {
-            return INSTANCE ?: synchronized(this){
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): SentenceRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     SentenceRoomDatabase::class.java,
                     "sentence_database"
-                ).build()
+                ).addCallback(SentenceDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 instance
             }
+        }
+    }
+
+    private class SentenceDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let {
+                scope.launch {
+                    populateDatabase(it.sentenceDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(sentenceDao: SentenceDao) {
+            //Delete all content here
+            sentenceDao.deleteAll()
+
+            //Add sample sentences
+            var sentence = SentenceEntity(
+                sentence = "My firs sentence"
+            )
+            sentenceDao.insert(sentence)
+
+            sentence = SentenceEntity(
+                sentence = "My second sentence"
+            )
+            sentenceDao.insert(sentence)
+
         }
     }
 }
