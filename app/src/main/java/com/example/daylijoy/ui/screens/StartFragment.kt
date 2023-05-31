726e9c1aeaf342bd1c53a606edcc43f312a9519b
Jhohells <jhohellserick@gmail.com>
2023-05-30 21:46:49 -0500

    navigation add, view and some

--- app/src/main/java/com/example/daylijoy/ui/screens/StartFragment.kt
+++ app/src/main/java/com/example/daylijoy/ui/screens/StartFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.daylijoy.R
+import com.example.daylijoy.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
 
+    private lateinit var binding: FragmentStartBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    ): View {
+        binding = FragmentStartBinding.inflate(inflater)
+
+        binding.btnStart.setOnClickListener {
+            val action = R.id.action_startFragment_to_sentencesFragment
+            it.findNavController().navigate(action)
+        }
+
+        return binding.root
     }
 
 }
