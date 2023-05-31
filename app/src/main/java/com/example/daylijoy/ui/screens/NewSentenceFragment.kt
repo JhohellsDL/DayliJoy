bfb204ef5dbca1accb3603cbf3f66f9c02dccfb6
Jhohells <jhohellserick@gmail.com>
2023-05-31 17:26:00 -0500

    snack bar add, and back navigation

--- app/src/main/java/com/example/daylijoy/ui/screens/NewSentenceFragment.kt
+++ app/src/main/java/com/example/daylijoy/ui/screens/NewSentenceFragment.kt
@@ -2,18 +2,24 @@ package com.example.daylijoy.ui.screens
 
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.daylijoy.DailyJoyApplication
+import com.example.daylijoy.R
 import com.example.daylijoy.data.entities.SentenceEntity
 import com.example.daylijoy.databinding.FragmentNewSentenceBinding
 import com.example.daylijoy.ui.viewmodels.SentenceViewModel
 import com.example.daylijoy.ui.viewmodels.SentenceViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -35,12 +41,14 @@ class NewSentenceFragment : Fragment() {
         }
 
         val dateFormat = getDate()
-
         val sentenceText = binding.editSentence.text
+        val action = R.id.action_newSentenceFragment_to_sentencesFragment
 
         binding.buttonSave.setOnClickListener {
             if (sentenceText.isNullOrBlank()) {
-                Toast.makeText(requireContext(), "vacio!", Toast.LENGTH_SHORT).show()
+                Snackbar.make(binding.root, R.string.empty_not_saved, Toast.LENGTH_SHORT)
+                    .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.colorAccent))
+                    .show()
             } else {
                 sentenceViewModel.insert(
                     SentenceEntity(
@@ -48,8 +56,11 @@ class NewSentenceFragment : Fragment() {
                         date = dateFormat
                     )
                 )
-                Toast.makeText(requireContext(), dateFormat, Toast.LENGTH_SHORT).show()
+                Snackbar.make(binding.root, R.string.sentence_saved, Toast.LENGTH_SHORT)
+                    .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.colorSecondary))
+                    .show()
             }
+            it.findNavController().navigate(action)
         }
 
         return binding.root
