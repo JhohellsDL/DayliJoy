3ca42f3ed789a8218d433618ff539fcf4c50d4b0
Jhohells <jhohellserick@gmail.com>
2023-06-01 23:10:56 -0500

    ads banner, ok

--- app/src/main/java/com/jdl/daylijoy/ui/screens/StartFragment.kt
+++ app/src/main/java/com/jdl/daylijoy/ui/screens/StartFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.jdl.daylijoy.R
 import com.jdl.daylijoy.databinding.FragmentStartBinding
 
@@ -17,6 +19,16 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStartBinding.inflate(inflater)
+
+        // Initialize the Mobile Ads SDK with an AdMob App ID.
+        MobileAds.initialize(this.requireContext()) {}
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background.
+        binding.adView.loadAd(adRequest)
+
 
         binding.btnStart.setOnClickListener {
             val action = R.id.action_startFragment_to_sentencesFragment
